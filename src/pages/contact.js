82532e08cbaa933a7ed2943b9db37533baf61233
Courtney Corlew <courtneyecorlew@gmail.com>
2019-11-15 19:58:58 -0500

    add more to contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,30 @@
 import React from "react"
 import PageLayout from "../components/pageLayout"
+import Twitter from "../images/twitter.inline.svg"
+import Github from "../images/github.inline.svg"
+import Instagram from "../images/instagram.inline.svg"
+
 import("../styles/index.scss")
 
 const Contact = () => (
   <PageLayout>
+    
     <div className="page-container">
       <span className="title">Contact</span>
       <div className="content">
-        <span>Feel free to use the form below to contact me or drop me a message at courtneycorlew@gmail.com.</span>
-
+        <h1>Hey! 👋 I can't wait to hear from you!</h1>
+        <span className="info">Feel free to use the form below to contact me or drop me a message at courtneycorlew@gmail.com.</span>
+        <div className="social-media-icons">
+          <a href="https://twitter.com/courtneyecorlew" target="_blank">
+            <Twitter />
+          </a>
+          <a href="https://github.com/courtneycorlew" target="_blank">
+            <Github />
+          </a>
+          <a href="https://instagram.com/courtneycorlew" target="_blank">
+            <Instagram />
+          </a>
+        </div>
         <form name="contact" action="/success/" method="post" data-netlify="true"
           data-netlify-honeypot="bot-field">
           <input type="hidden" name="bot-field" />
